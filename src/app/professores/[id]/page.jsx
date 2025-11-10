a5eda38f20e38162212207a59165131d842e1b59
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-10 06:29:54 -0300

    feat(hooks): Create useProfessor hook for improved data management in Professor component

--- src/app/professores/[id]/page.jsx
+++ src/app/professores/[id]/page.jsx
@@ -1,21 +1,14 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import { getProfessor } from '@/store/slices/professoresSlice';
+import { useProfessor } from '@/hooks/professores/useProfessor';
 
 export default function Professor() {
   const params = useParams();
-  const dispatch = useDispatch();
-  const { current, message, loading } = useSelector(state => state.professores);
-
-  useEffect(() => {
-    if (params.id) {
-      dispatch(getProfessor(params.id));
-    }
-  }, [dispatch, params.id]);
+  const { professor, message, isLoading, isSuccess, isFailed } = useProfessor(
+    params.id
+  );
 
   return (
     <div className="p-10">
@@ -36,28 +29,30 @@ export default function Professor() {
         </Link>
       </div>
 
-      {loading && <p className="text-blue-600">Carregando...</p>}
+      {isLoading && <p className="text-blue-600">Carregando...</p>}
 
-      {message && (
+      {isFailed && (
         <div
           className={`p-4 rounded-md mb-4 ${
-            current ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
+            professor
+              ? 'bg-green-100 text-green-800'
+              : 'bg-red-100 text-red-800'
           }`}
         >
           {message}
         </div>
       )}
 
-      {current && (
+      {isSuccess && professor && (
         <div>
-          <p>Id: {current.id}</p>
-          <p>Nome: {current.nome}</p>
-          <p>Sobrenome: {current.sobrenome}</p>
-          <p>Email: {current.email}</p>
-          <p>Telefone: {current.telefone}</p>
-          <p>Permissão: {current.permissao}</p>
-          <p>Data de criação: {current.dataCriacao}</p>
-          <p>Data de atualização: {current.dataAtualizacao}</p>
+          <p>Id: {professor.id}</p>
+          <p>Nome: {professor.nome}</p>
+          <p>Sobrenome: {professor.sobrenome}</p>
+          <p>Email: {professor.email}</p>
+          <p>Telefone: {professor.telefone}</p>
+          <p>Permissão: {professor.permissao}</p>
+          <p>Data de criação: {professor.dataCriacao}</p>
+          <p>Data de atualização: {professor.dataAtualizacao}</p>
         </div>
       )}
     </div>
