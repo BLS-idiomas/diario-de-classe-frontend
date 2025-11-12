b44897e710d5acac4d0805b820353e4c28e00a58
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-12 09:55:08 -0300

    refactor: Enhance sidebar state management and responsiveness in Layout component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,25 +2,46 @@
 
 import { useState } from 'react';
 import { Header, Sidebar, Footer } from '..';
+import { isMobileFunction } from '@/utils/isMobileFunction';
 
 export const Layout = ({ children }) => {
-  const [sidebarExpanded, setSidebarExpanded] = useState(false);
+  const [sidebarExpanded, setSidebarExpanded] = useState({
+    mainClass: 'ml-18',
+    sidebarClass: 'w-18',
+    isExpanded: false,
+  });
 
   const toggleSidebar = () => {
-    setSidebarExpanded(!sidebarExpanded);
+    const isMobile = isMobileFunction();
+    let mainClass = 'ml-18';
+    let sidebarClass = 'w-18';
+
+    if (!sidebarExpanded.isExpanded) {
+      if (isMobile === true) {
+        sidebarClass = 'absolute w-full';
+      } else {
+        sidebarClass = 'w-[180px]';
+        mainClass = 'ml-[150px]';
+      }
+    }
+
+    setSidebarExpanded({
+      mainClass: mainClass,
+      sidebarClass: sidebarClass,
+      isExpanded: !sidebarExpanded.isExpanded,
+    });
   };
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <div className="flex pt-16 min-h-screen">
         <Sidebar
-          sidebarExpanded={sidebarExpanded}
+          sidebarExpanded={sidebarExpanded.isExpanded}
+          sidebarClass={sidebarExpanded.sidebarClass}
           toggleSidebar={toggleSidebar}
         />
         <main
-          className={`flex-1 transition-all duration-300 ease-in-out ${
-            sidebarExpanded ? 'ml-[150px]' : 'ml-10'
-          }`}
+          className={`flex-1 transition-all duration-300 ease-in-out ${sidebarExpanded.mainClass}`}
         >
           {children}
         </main>
