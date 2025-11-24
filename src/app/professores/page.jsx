46be1ca032ff7226cc8375371511da2cd6473a9d
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-11-24 09:47:03 -0300

    refactor: refatoração

--- src/app/professores/page.jsx
+++ src/app/professores/page.jsx
@@ -6,52 +6,19 @@ import { Eye, Pencil, Trash2 } from 'lucide-react';
 import { useProfessores } from '@/hooks/professores/useProfessores';
 import { useDeletarProfessor } from '@/hooks/professores/useDeletarProfessor';
 import { useFormater } from '@/hooks/useFormater';
+import { useProfessoresList } from '@/hooks/professores/useProfessoresList';
 import { Table } from '@/components';
 
 export default function Professores() {
-  const { professores, isLoading, isSuccess, isEmpty, columns } =
-    useProfessores();
+  const { professores, isLoading } = useProfessores();
   const { handleDeleteProfessor } = useDeletarProfessor();
   const { telefoneFormatter, dataFormatter } = useFormater();
-
-  const data = useMemo(() => {
-    const strokeWidth = 1;
-    const size = 16;
-    const px = 2;
-    return professores.map(prof => ({
-      id: prof.id,
-      name: prof.nome,
-      sobrenome: prof.sobrenome,
-      telefone: telefoneFormatter(prof.telefone),
-      email: prof.email,
-      role: prof.permissao,
-      dataCriacao: dataFormatter(prof.dataCriacao),
-      acoes: (
-        <div className="flex gap-2">
-          <Link
-            href={`/professores/${prof.id}`}
-            className={`px-${px} py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-600`}
-          >
-            <Eye strokeWidth={strokeWidth} size={size} />
-          </Link>
-
-          <Link
-            href={`/professores/${prof.id}/editar`}
-            className={`px-${px} py-1 text-sm bg-gray-500 text-white rounded hover:bg-gray-600`}
-          >
-            <Pencil strokeWidth={strokeWidth} size={size} />
-          </Link>
-
-          <button
-            onClick={() => handleDeleteProfessor(prof.id)}
-            className={`px-${px} py-1 text-sm bg-red-500 text-white rounded hover:bg-red-600`}
-          >
-            <Trash2 strokeWidth={strokeWidth} size={size} />
-          </button>
-        </div>
-      ),
-    }));
-  }, [professores, telefoneFormatter, dataFormatter, handleDeleteProfessor]);
+  const { columns, data } = useProfessoresList({
+    professores,
+    telefoneFormatter,
+    dataFormatter,
+    handleDeleteProfessor,
+  });
 
   return (
     <div className="p-6 mx-auto bg-white shadow rounded-lg">
--- src/hooks/professores/useProfessores.js
+++ src/hooks/professores/useProfessores.js
@@ -12,59 +12,10 @@ export function useProfessores() {
   }, [dispatch]);
 
   const isLoading = status === STATUS.IDLE || status === STATUS.LOADING;
-  const isSuccess = status === STATUS.SUCCESS;
-  const isEmpty = isSuccess && list && list.length === 0;
-  const columns = [
-    {
-      name: '#',
-      selector: row => row.id,
-      sortable: true,
-      width: '75px',
-    },
-    {
-      name: 'Nome',
-      selector: row => row.name,
-      sortable: true,
-    },
-    {
-      name: 'Sobrenome',
-      selector: row => row.sobrenome,
-      sortable: true,
-    },
-    {
-      name: 'Telefone',
-      selector: row => row.telefone,
-      sortable: true,
-    },
-    {
-      name: 'Email',
-      selector: row => row.email,
-      sortable: true,
-    },
-    {
-      name: 'Permissão',
-      selector: row => row.role,
-      sortable: true,
-    },
-    {
-      name: 'Data de criação',
-      selector: row => row.dataCriacao,
-      sortable: true,
-    },
-    {
-      name: 'Ações',
-      selector: row => row.acoes,
-      sortable: false,
-      width: 'auto',
-    },
-  ];
 
   return {
     professores: list,
     status,
     isLoading,
-    isSuccess,
-    isEmpty,
-    columns,
   };
 }
