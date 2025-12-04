e862a75a383972fa651ecf6875246efccf9308a6
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-04 09:40:01 -0300

    feat: implement EditarAluno and NovoAluno pages with form handling and error management

--- src/app/(application)/alunos/[id]/page.jsx
+++ src/app/(application)/alunos/[id]/page.jsx
@@ -1,3 +1,40 @@
+'use client';
+
+import { useEffect } from 'react';
+import Link from 'next/link';
+import { notFound, useParams } from 'next/navigation';
+import { STATUS_ERROR } from '@/constants/statusError';
+import { useFormater } from '@/hooks/useFormater';
+import { useAluno } from '@/hooks/alunos/useAluno';
+import {
+  Container,
+  PageContent,
+  PageTitle,
+  PageSubTitle,
+  ButtonGroup,
+  Loading,
+  Section,
+  Table,
+} from '@/components';
+
 export default function Aluno() {
+  const params = useParams();
+  const { aluno, aulas, diasAulas, contrato, isLoading, statusError } =
+    useAluno(params.id);
+  const { telefoneFormatter, dataFormatter } = useFormater();
+
+  useEffect(() => {
+    if (
+      statusError === STATUS_ERROR.BAD_REQUEST ||
+      statusError === STATUS_ERROR.NOT_FOUND
+    ) {
+      return notFound();
+    }
+  }, [statusError]);
+
+  if (isLoading || !aluno) {
+    return <Loading />;
+  }
+
   return <h1>Aluno Page</h1>;
 }
