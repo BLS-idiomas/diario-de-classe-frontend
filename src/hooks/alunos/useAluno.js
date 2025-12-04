57ed04dc1af12781744c9e325d772f8ecb887404
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-04 08:49:38 -0300

    feat: enhance useAluno hook and tests to include fetching aulas, dias aulas, and contrato

--- src/hooks/alunos/useAluno.js
+++ src/hooks/alunos/useAluno.js
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAluno } from '@/store/slices/alunosSlice';
+import {
+  getAluno,
+  getAulasAluno,
+  getDiasAulasAluno,
+  getContratoAluno,
+} from '@/store/slices/alunosSlice';
 import { STATUS } from '@/constants';
 
 export function useAluno(id) {
   const dispatch = useDispatch();
-  const { current, message, status, statusError } = useSelector(
-    state => state.alunos
-  );
+  const { current, aulas, diasAulas, contrato, message, status, statusError } =
+    useSelector(state => state.alunos);
   const isLoading = status === STATUS.IDLE || status === STATUS.LOADING;
   const isSuccess = status === STATUS.SUCCESS;
   const isFailed = status === STATUS.FAILED;
@@ -15,11 +19,17 @@ export function useAluno(id) {
   useEffect(() => {
     if (id) {
       dispatch(getAluno(id));
+      dispatch(getAulasAluno(id));
+      dispatch(getDiasAulasAluno(id));
+      dispatch(getContratoAluno(id));
     }
   }, [dispatch, id]);
 
   return {
     aluno: current,
+    aulas,
+    diasAulas,
+    contrato,
     message,
     isLoading,
     isSuccess,
