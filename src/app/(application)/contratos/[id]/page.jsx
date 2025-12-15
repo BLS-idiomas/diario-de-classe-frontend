00ecabab8e060c5b72f6a3066c187470ab9c1b26
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-15 06:47:05 -0300

    feat: enhance Contrato component with aluno details and additional info cards

--- src/app/(application)/contratos/[id]/page.jsx
+++ src/app/(application)/contratos/[id]/page.jsx
@@ -12,13 +12,18 @@ import {
   ButtonGroup,
   Loading,
   Section,
+  InfoCardGroup,
+  InfoCard,
+  HeaderAvatar,
+  BadgeGroup,
+  Badge,
 } from '@/components';
 import { useContrato } from '@/hooks/contratos/useContrato';
 
 export default function Contrato() {
   const params = useParams();
-  const { contrato, isLoading, isNotFound } = useContrato(params.id);
-  const { telefoneFormatter, dataFormatter } = useFormater();
+  const { aluno, contrato, isLoading, isNotFound } = useContrato(params.id);
+  const { dataFormatter } = useFormater();
 
   useEffect(() => {
     if (isNotFound) {
@@ -29,6 +34,8 @@ export default function Contrato() {
   if (isLoading || !contrato) {
     return <Loading />;
   }
+
+  console.log(contrato);
 
   return (
     <Container>
@@ -49,6 +56,51 @@ export default function Contrato() {
           Editar
         </Link>
       </ButtonGroup>
+
+      <div className="mt-4 space-y-8">
+        <Section>
+          {/* Header: avatar + name/email */}
+          {aluno && <HeaderAvatar entity={aluno} />}
+          <BadgeGroup>
+            <Badge
+              icon="calendar"
+              color="gray"
+              text={`Total de aulas ${contrato.totalAulas}`}
+            />
+            <Badge
+              icon={contrato.status === 'ATIVO' ? 'check' : 'alert'}
+              color="gray"
+              text={`Situação ${contrato.status}`}
+            />
+          </BadgeGroup>
+          <InfoCardGroup>
+            {/* Datas */}
+            <InfoCard
+              columns={[
+                { text: 'Vigência do contrato', type: 'header' },
+                { text: dataFormatter(contrato.dataInicio) },
+                { text: dataFormatter(contrato.dataTermino) },
+              ]}
+            />
+            {/* Aulas realizadas */}
+            <InfoCard
+              columns={[
+                { text: 'Aulas realizadas', type: 'header' },
+                { text: `Aulas comum ${contrato.totalAulasFeitas}` },
+                { text: `Aulas de reposição ${contrato.totalReposicoes}` },
+              ]}
+            />
+            {/* Aulas não realizadas */}
+            <InfoCard
+              columns={[
+                { text: 'Aulas não realizadas', type: 'header' },
+                { text: `Aulas canceladas ${contrato.totalAulasCanceladas}` },
+                { text: `Faltas ${contrato.totalFaltas}` },
+              ]}
+            />
+          </InfoCardGroup>
+        </Section>
+      </div>
     </Container>
   );
 }
