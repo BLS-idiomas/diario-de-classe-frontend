0c611970cef333dd34077c550e5f9fc378a84d40
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-15 05:01:50 -0300

    feat: create Contratos page component for listing and managing contracts

---
+++ src/app/(application)/contratos/page.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Link from 'next/link';
+import { useUserAuth } from '@/providers/UserAuthProvider';
+import { useFormater } from '@/hooks/useFormater';
+import { ButtonGroup, Container, PageTitle, Table } from '@/components';
+import { useContratos } from '@/hooks/contratos/useContratos';
+import { useDeletarContrato } from '@/hooks/contratos/useDeletarContrato';
+import { useContratosList } from '@/hooks/contratos/useContratosList';
+
+export default function Contratos() {
+  const { currentUser } = useUserAuth();
+  const { contratos, isLoading } = useContratos();
+  const { handleDeleteContrato } = useDeletarContrato();
+  const { dataFormatter } = useFormater();
+  const { columns, data } = useContratosList({
+    currentUser,
+    contratos,
+    readOnly: false,
+    dataFormatter,
+    handleDeleteContrato,
+  });
+
+  return (
+    <Container>
+      <PageTitle>Lista de Contratos</PageTitle>
+
+      <ButtonGroup>
+        <Link href="/contratos/novo" className="btn btn-primary">
+          Novo contrato
+        </Link>
+      </ButtonGroup>
+
+      <Table
+        columns={columns}
+        data={data}
+        isLoading={isLoading}
+        notFoundMessage="Nenhum aluno encontrado."
+      />
+    </Container>
+  );
+}
