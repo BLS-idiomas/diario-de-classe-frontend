5dc362bc54f539c1d6d174acbe8aba404c6ac29f
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-22 07:48:19 -0300

    fix: add TODO comment to improve sidebar color in HeaderAvatar component

--- src/components/app/details/HeaderAvatar/index.jsx
+++ src/components/app/details/HeaderAvatar/index.jsx
@@ -3,6 +3,7 @@ import { Avatar } from '@/components/ui';
 export const HeaderAvatar = ({ entity }) => {
   const nomeCompleto = `${entity.nome} ${entity.sobrenome}`;
   const { email } = entity;
+  // TODO melhora cor da sidebar
   return (
     <div className="flex items-center gap-4 mb-3">
       <Avatar text={nomeCompleto} />
