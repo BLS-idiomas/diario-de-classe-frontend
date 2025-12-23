c18191a99f0847c28fb529cd9886d490c1cfe030
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-12-23 09:58:48 -0300

    feat: enhance Loading component to accept customizable size and border props

--- src/components/ui/Loading/index.jsx
+++ src/components/ui/Loading/index.jsx
@@ -1,10 +1,12 @@
-export const Loading = () => {
+export const Loading = ({ size = 12, border = 4 }) => {
   return (
     <div
       data-testid="loading"
       className="flex items-center justify-center h-full w-full"
     >
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
+      <div
+        className={`animate-spin rounded-full h-${size} w-${size} border-t-${border} border-b-${border} border-blue-500`}
+      ></div>
     </div>
   );
 };
