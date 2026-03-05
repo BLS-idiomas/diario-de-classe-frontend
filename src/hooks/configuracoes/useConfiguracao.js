e478f2d4509916a8efa08beb6d5032dbb3d17df3
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2026-03-05 06:44:21 -0300

    feat: implement useConfiguracao hook with state management and side effects

---
+++ src/hooks/configuracoes/useConfiguracao.js
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { STATUS } from '@/constants';
+import {
+  getConfiguracao,
+  updateConfiguracao,
+  clearStatus,
+} from '@/store/slices/configuracaoSlice';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/providers/ToastProvider';
+
+export function useConfiguracao() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { success } = useToast();
+  const { data, status, action, message, errors } = useSelector(
+    state => state.configuracao
+  );
+  const isLoading =
+    (status === STATUS.IDLE || status === STATUS.LOADING) &&
+    action === 'getConfiguracao';
+  const isSubmitting =
+    status === STATUS.LOADING && action === 'updateConfiguracao';
+
+  const submit = dataToSend => {
+    dispatch(updateConfiguracao(dataToSend));
+  };
+
+  useEffect(() => {
+    dispatch(getConfiguracao());
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log('feito', { status, action, data });
+    if (status === STATUS.SUCCESS && data && action === 'updateConfiguracao') {
+      dispatch(clearStatus());
+      success('Operação realizada com sucesso!');
+      router.push(`/configuracoes`);
+    }
+  }, [status, router, success, data, action, dispatch]);
+
+  return {
+    submit,
+    configuracao: data,
+    status,
+    isLoading,
+    message,
+    errors,
+    isSubmitting,
+  };
+}
